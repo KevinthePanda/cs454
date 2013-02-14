6b2d4307d4d17348ad6a514c3ff4d1d77656dfb0
Maverick <maverick.lee225@gmail.com>
2013-02-14 15:49:37 -0500

    get shit to connect

--- a2/stringClient.cpp
+++ a2/stringClient.cpp
@@ -1,9 +1,13 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 using namespace std;
@@ -41,6 +45,25 @@ int main() {
 
   pthread_t thread;
   pthread_create(&thread, NULL, SendToServer, &buffer);
+
+  int sockfd, portno, n;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+  portno = atoi(port);
+  sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  server = gethostbyname(serverAddress);
+
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char *)server->h_addr,
+        (char *)&serv_addr.sin_addr.s_addr,
+        server->h_length);
+  serv_addr.sin_port = htons(portno);
+
+  connect(sockfd,(struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+
+  printf("SUCCESS !!! Connection established \n");
 
   while (true) {
     getline(cin, data);
