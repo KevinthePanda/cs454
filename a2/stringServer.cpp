ec9ab1fae75d0f283b023a6acb4e261b91de2f69
Maverick <maverick.lee225@gmail.com>
2013-02-13 20:39:52 -0500

    create title case function

--- a2/stringServer.cpp
+++ a2/stringServer.cpp
@@ -1,5 +1,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 /*
 #include "stringServer.h"
@@ -8,6 +12,17 @@ StringServer::StringServer() {
 }
 */
 
-int main() {
+string titleCase(string str) {
+  for (unsigned int i = 0; i < str.length(); i++) {
+    if (i == 0 || str[i-1] == ' ') {
+      str[i] = toupper(str[i]);
+    } else {
+      str[i] = tolower(str[i]);
+    }
+  }
+  return str;
+}
 
+int main() {
+  cout << titleCase("test SDFLK kljsdfSDfjs $a") << endl;
 }
