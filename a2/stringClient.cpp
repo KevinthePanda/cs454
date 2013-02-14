4bb162a70c5c0c08ddf12854d37594752076ae6c
Maverick <maverick.lee225@gmail.com>
2013-02-13 21:18:20 -0500

    blah

--- a2/stringClient.cpp
+++ a2/stringClient.cpp
@@ -1,6 +1,12 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <iostream>
+using namespace std;
 
 int main() {
-
+  string data;
+  while (true) {
+    cin >> data;
+    cout << "Server: " << data << endl;
+  }
 }
