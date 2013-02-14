085fb5f103863d83b93a02a03eeb2b248ec15c99
Maverick <maverick.lee225@gmail.com>
2013-02-13 22:10:47 -0500

    getenv for client

--- a2/stringClient.cpp
+++ a2/stringClient.cpp
@@ -1,10 +1,15 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <iostream>
+#include <string>
 #include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
 int main() {
+  char *serverAddress = getenv("SERVER_ADDRESS");
+  char *port = getenv("SERVER_PORT");
   string data;
   vector<string> buffer;
   while (true) {
