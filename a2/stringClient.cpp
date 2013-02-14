3f92a68bb9e36de63117245e5b543e8b6b041339
Maverick <maverick.lee225@gmail.com>
2013-02-13 21:25:58 -0500

    add a buffer

--- a2/stringClient.cpp
+++ a2/stringClient.cpp
@@ -1,12 +1,17 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
   string data;
+  vector<string> buffer;
   while (true) {
-    cin >> data;
-    cout << "Server: " << data << endl;
+    getline(cin, data);
+    buffer.push_back(data);
+    string blah = buffer[0];
+    buffer.erase(buffer.begin());
+    cout << "Server: " << blah << endl;
   }
 }
